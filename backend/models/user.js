c8c95baf49b6e0cf56ceb6396fc16eed809ef2c2
KhushalCoder29 <khushal.acharya29@gmail.com>
2026-05-09 18:04:58 +0530

    mongodburi changed commit

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   skinType: { type: String, required: true },
-  
+
   // This will store the 4-step array Gemini generates
   savedRoutine: { type: Array, default: [] }, 
-  
+
   // Gamification tracking
   currentStreak: { type: Number, default: 0 },
   points: { type: Number, default: 0 },
